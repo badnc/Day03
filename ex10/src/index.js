f70fef6668bd0be930f722dd69998172cfe02014
badnc <79924957+badnc@users.noreply.github.com>
2021-03-10 18:41:26 +0100

    Update index.js

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -10,6 +10,8 @@ function main(numOne, numTwo) {
     
 }
 
-console.log(main(15, 3),(25, 8),(425, 10));
+console.log(main(15, 3));
+console.log(main(25, 8));
+console.log(main(425, 10));
 
 module.exports = main;
