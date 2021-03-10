92622348d422d7d6208b22a388869a8d0ac3899f
badnc <79924957+badnc@users.noreply.github.com>
2021-03-10 17:43:06 +0100

    Update index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -10,8 +10,6 @@ function main(numOne, numTwo) {
     
 }
 
-console.log(main(15, 8));
-console.log(main(5, 28));
-console.log(main(150, 425));
+console.log(main(15, 8),(5, 28),(150, 425));
 
 module.exports = main;
